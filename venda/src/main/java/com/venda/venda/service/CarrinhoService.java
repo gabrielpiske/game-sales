44458d7212cdac05ee29308f424445284876152a
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-12-14 16:42:57 -0300

    add: carrinhoService

--- venda/src/main/java/com/venda/venda/service/CarrinhoService.java
+++ venda/src/main/java/com/venda/venda/service/CarrinhoService.java
@@ -1,8 +1,11 @@
 package com.venda.venda.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.venda.venda.model.Carrinho;
 import com.venda.venda.repository.CarrinhoRepository;
 
 @Service
@@ -11,5 +14,19 @@ public class CarrinhoService {
     @Autowired
     private CarrinhoRepository carrinhoRepository;
 
-    
+    public List<Carrinho> listarTodos() {
+        return carrinhoRepository.findAll();
+    }
+
+    public Carrinho salvar(Carrinho carrinho) {
+        return carrinhoRepository.save(carrinho);
+    }
+
+    public Carrinho buscarPorId(Integer id) {
+        return carrinhoRepository.findById(id).orElse(null);
+    }
+
+    public void deletarPorId(Integer id) {
+        carrinhoRepository.deleteById(id);
+    }
 }
